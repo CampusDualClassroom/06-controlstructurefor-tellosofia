b44173e4d64dcd95f323798a38bffd5c25deda62
Sofía Tello <tellosofia@outlook.com>
2025-10-20 16:24:08 +0200

    Exercise 6: Solved

--- src/main/java/com/campusdual/classroom/Exercise.java
+++ src/main/java/com/campusdual/classroom/Exercise.java
@@ -12,13 +12,24 @@ public class Exercise {
     //TODO ↓
     // Que imprima como mensaje la suma de los primeros N números positivos
     public static void sumFirstNaturalNumbers(int num) {
+        int sum_num = 0;
 
+        for (int i = 0; i <= num; i++) {
+            sum_num = sum_num + i;
+        }
+        System.out.println(sum_num);
     }
 
     //TODO ↓
     // Que imprima por pantalla los N primeros números positivos
     public static void showFirstNaturalNumbers(int num) {
+        int show_num = 0;
 
-    }
+        for (int i = 0; i <= num; i++) {
+            System.out.println(show_num);
+            show_num++;
+        }
+        }
+
 
 }
